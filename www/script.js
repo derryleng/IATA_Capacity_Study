3f5238187643fa0c8b82276561df93f7c1ba705b
Derry Leng <derry.leng@gmail.com>
2019-01-28 09:09:41 +0000

    v4.0.1
    Set plot size update delay on window resize

--- www/script.js
+++ www/script.js
@@ -1,13 +1,31 @@
 var dimension = [0, 0];
 
-$(document).on("shiny:connected", function(e) {
+$(document).on("shiny:connected", function() {
   dimension[0] = $(".content-wrapper").width();
   dimension[1] = $(".content-wrapper").height();
   Shiny.onInputChange("dimension", dimension);
 });
 
-$(window).resize(function(e) {
-  dimension[0] = $(".content-wrapper").width();
-  dimension[1] = $(".content-wrapper").height();
-  Shiny.onInputChange("dimension", dimension);
+var rtime;
+var timeout = false;
+var delta = 100;
+
+$(window).resize(function() {
+  rtime = new Date();
+  if (timeout === false) {
+    timeout = true;
+    setTimeout(resizeend, delta);
+  }
 });
+
+function resizeend() {
+  if (new Date() - rtime < delta) {
+    setTimeout(resizeend, delta)
+  } else {
+    timeout = false;
+    dimension[0] = $(".content-wrapper").width();
+    dimension[1] = $(".content-wrapper").height();
+    Shiny.onInputChange("dimension", dimension);
+  }
+}
+
