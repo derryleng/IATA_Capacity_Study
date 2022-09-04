37545d105a1d865fc482d6689d0fd615325023bf
Derry Leng <derry.leng@gmail.com>
2022-09-04 22:28:22 +0100

    Delete secret.R

--- secret.R
+++
@@ -1,4 +0,0 @@
-Sys.setenv("plotly_username"="rob.sawyer")
-Sys.setenv("plotly_api_key"="HSQo0QjxFICKCIsnCPqW")
-# Sys.setenv("plotly_username"="derry.leng")
-# Sys.setenv("plotly_api_key"="dxWgmpWjP3vkfcTQDZje")
